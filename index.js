d50a703eedb9d2664961190d469a729029f01275
Alexander Fedotov <alexander.g.fedotov@kaspersky.com>
2017-10-30 16:48:37 +0300

    Added environment port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let express = require('express'),
 	app = express();
 
+app.set('port', (process.env.PORT || 3199));
 app.use(express.static(__dirname + '/frontend'));
 app.all('*', function(req, res) {
 	// Just send the index.html for other files to support HTML5Mode
 	res.sendFile('/frontend/index.html', { 'root': __dirname });
 });
 
-app.listen(3199, function() {
+app.listen(app.get('port'), function() {
 	console.log('Server started on port 3199');
 });
